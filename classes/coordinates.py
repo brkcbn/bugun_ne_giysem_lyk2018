1f2ffb4aec3232d943c307bf9e625bd10deffe6a
kiraci <osmanbugraaydin@gmail.com>
2018-08-02 15:30:17 +0300

    İp bulma sorunu düzeltildi ve kordinatlar return edildi

--- classes/coordinates.py
+++ classes/coordinates.py
@@ -1,14 +1,23 @@
 import requests
 import json
-
-
 class Coordinates():
+    latitude = ""
+    longitude = ""
     ip = None
-
     def find_ip(self):
-        import socket
-        self.ip = socket.gethostbyname(socket.gethostname())
+        r = requests.get(r'http://jsonip.com')
+        self.ip = r.json()['ip']
+        print('Your IP is', self.ip)
+    def give_my_coordinates(self):
+        ham_data = requests.get("http://api.ipstack.com/{}?access_key=a5966744b2c34ab0d19f2cf74c440dd6".format(self.ip)).json()
+        print(ham_data)
+
+        self.latitude = ham_data['latitude']
+        self.longitude = ham_data['longitude']
+        return self.latitude , self.longitude
 
 
 api = Coordinates()
 api.find_ip()
+api.give_my_coordinates()
+

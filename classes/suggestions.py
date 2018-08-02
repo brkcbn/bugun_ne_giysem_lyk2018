db50366687bf992f53fb79e1287cd3d851718760
Elis Güler <gulerelis@gmail.com>
2018-08-02 14:25:51 +0300

    önerilistesi

--- classes/suggestions.py
+++ classes/suggestions.py
@@ -1,5 +1,11 @@
 class Suggestions():
     def __init__():
+        temperature = []
+        precipitation = []
+        clothe_winter = ["cap", "scarf", "glove", "umbrella", ]
+        clothe_summer = ["hat", "flipflops",  ]
+        clothe_autumn = ["rain boots", "umbrella", ]
+
         print("suggestion")
         
         

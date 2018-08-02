6fb87debed6866895dc23ecbeb427c4e2268ab18
brkcbn <burakcoban48@gmail.com>
2018-08-02 16:34:34 +0300

    Sugg_tamamlandi_control_edin

--- classes/suggestions.py
+++ classes/suggestions.py
@@ -1,45 +1,52 @@
 class Suggestions():
     temperature = None
     precipitation = None
+
     def __init__(self, precipitation, temperature):
         self.temperature = temperature
         self.precipitation = precipitation
         clothe_too_cold = ["scarf", "glove", "thermals", "boot"]
-        clothe_cold = ["scarf", "cap" "umbrella" ]
-        clothe_normal = ["rain boots", "umbrella" ]
-        clothe_hot = [ "sunglasses" , "light clothes"]
+        clothe_cold = ["scarf", "cap","umbrella"]
+        clothe_rain = ["rain boots", "umbrella"]
+        clothe_hot = ["sunglasses", "light clothes"]
         clothe_too_hot = ["hat", "flip-flops", "sun glasses", "light clothes"]
 
-        if precipitation is True :
+        if precipitation:
 
             if precipitation == "Extreme":
-                print("Take your {}, {}; wear your {} and {}.".format(clothe_too_cold))
+                print("Take your {} and {}, wear your {} and {}.".format(*clothe_too_cold))
 
-            if precipitation == "Snow" :
-                    pass
+            elif precipitation == "Snow":
+                print("Wear your {} and {}, do not forget to take an {}.".format(*clothe_cold))
 
-            if precipitation == "Rain" :
-                    pass
+            elif precipitation == "Rain":
+                print("Wear your {}, do not forget to take an {}.".format(*clothe_rain))
 
-
-            else:
-                print("There is no precipitation")
-
-        Extreme
-        if self.temperature.isnumeric:
-            self.temperature = int(self.temperature)
-            if self.temperature <= 0 :
-                pass
-            elif self.temperature > 0 and self.temperature <= 13:
-                pass
-            elif self.temperature > 13 and self.temperature <= 24:
-                pass
-            elif self.temperature > 24 and self.temperature <= 35:Extreme
-                pass
-            elif self.temperature > 35:
-                pass
 
         else:
-            print("Temperature variable has not numeric value !")
+            print("There is no precipitation")
 
-sd = Suggestions("Extreme",24)
+
+        if self.temperature.isnumeric:
+            self.temperature = int(self.temperature)
+
+            if self.temperature <= 0:
+                print("Be careful while driving or walking!")
+
+            elif self.temperature > 0 and self.temperature <= 13:
+                print("Spring is coming")
+
+            elif self.temperature > 13 and self.temperature <= 24:
+                print("Mont Beni")
+
+            elif self.temperature > 24 and self.temperature <= 35:
+                print("Yaz geldi, her gece sokakta")
+
+            elif self.temperature > 35:
+                print("Esmiyooor!")
+
+        else:
+            print("Temperature variable is not a string and has not a numeric value !")
+
+
+sd = Suggestions("Snow", "-5")

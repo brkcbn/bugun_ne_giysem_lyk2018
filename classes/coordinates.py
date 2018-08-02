9e2d69c143b4eb2800a97f6891b59e98980835f7
kiraci <osmanbugraaydin@gmail.com>
2018-08-02 14:47:08 +0300

    İp bulundu

--- classes/coordinates.py
+++ classes/coordinates.py
@@ -1,7 +1,14 @@
+import requests
+import json
+
+
 class Coordinates():
-   def __init__(self):
-        print("coordinates")
-        
-    
-    #get coordinate
-        #return coordinate
+    ip = None
+
+    def find_ip(self):
+        import socket
+        self.ip = socket.gethostbyname(socket.gethostname())
+
+
+api = Coordinates()
+api.find_ip()
